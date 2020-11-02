91deb4a92f3b2d6dbfce9d3517bbdc808311ad70
Mmaud <maudlemao@gmail.com>
2020-11-02 12:08:26 +0100

    adding throw statements to getHigherNumber

--- notions_de_base/exercice1.js
+++ notions_de_base/exercice1.js
@@ -1,7 +1,7 @@
 const getHigherNumber = (integersArray) => {
     let higherNumber = null;
-    if (!isAnArray(integersArray)) console.log("Your parameter must be an array");
-    if (containIntegersOnly(integersArray) === false) console.log("Your array must contain numbers only");
+    if (!isAnArray(integersArray)) throw 'Your parameter must be an array';
+    if (!containIntegersOnly(integersArray)) throw 'Your array must contain numbers only';
     
     integersArray.forEach(integer => {
         if (integer >= higherNumber)  higherNumber = integer;
