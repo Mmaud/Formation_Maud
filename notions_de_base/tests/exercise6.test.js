49b4bbf6905a18fbd4c633c354524c864039fac7
Mmaud <maudlemao@gmail.com>
2020-11-03 16:02:12 +0100

    created and tested sortCardsByPrice

--- notions_de_base/tests/exercise6.test.js
+++ notions_de_base/tests/exercise6.test.js
@@ -1,12 +1,23 @@
 const getObjectList = require('../exercice3');
-const sortsCardsByPrice = require('../exercise6');
+const sortCardsByPrice = require('../exercise6');
 
-describe('sortsCardsByPrice sort the card by prices', () => {
-    test('sortsCardsByPrice must return the card with this order : chaise table canape', () => {
+describe('sortCardsByPrice sort the card by prices', () => {
+    test('sortCardsByPrice must return the card with this order : chaise table canape', () => {
         let cards = [{ quantity: 8, item: { name: 'chaise', price: 10, } },
         { quantity: 2, item: { name: 'table', price: 50, } },
         { quantity: 1, item: { name: 'canape', price: 200, } }];
-        let sortedCards = sortsCardsByPrice(cards);
+        let sortedCards = sortCardsByPrice(cards);
         expect(getObjectList(sortedCards)).toStrictEqual([ 'chaise', 'table','canape']);
+    })
+})
+
+describe('testing sortCardsByPrice error statements', () => {
+    test('throw an error when parameter is empty', () => {
+        expect(() => sortCardsByPrice([])).toThrow('Your paramater is empty, it must contain a card');
+    })
+    test('throw an error when price is invalid', () => {
+        let cardsWithWrongPrice = [{ quantity: 8, item: { name: 'chaise', price: 10, } },
+        { quantity: 2, item: { name: 'table', price: undefined, } }];
+        expect(() => sortCardsByPrice(cardsWithWrongPrice)).toThrow('Some price item is invalid');
     })
 })
